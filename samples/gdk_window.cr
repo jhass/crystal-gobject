dbb5ec2140396f06c3e6dc1f87ce1a401dd8ecfe
Jonne Haß <jonne.hass@offerista.com>
2020-04-29 00:09:59 +0200

    Gdk window sample: listen to delete event

--- samples/gdk_window.cr
+++ samples/gdk_window.cr
@@ -9,7 +9,7 @@ window = Gdk::Window.new(nil,
     wclass: Gdk::WindowWindowClass::INPUT_OUTPUT,
     width: 800,
     height: 600
-    ),
+  ),
   Gdk::WindowAttributesType.flags(TITLE)
 )
 
@@ -17,15 +17,20 @@ window.events = Gdk::EventMask::ZERO_NONE
 
 window.show
 
+main_loop = GLib::MainLoop.new(nil, true)
+
 Gdk::Event.on_event do |event|
   case event.event_type
   when .expose?
     puts "Expose count: #{event.expose.count}"
   when .setting?
     puts "Setting #{event.setting.name} is #{event.setting.action}"
+  when .delete?
+    puts "Received window delete"
+    main_loop.quit
   else
     puts "Unhandled event: #{event.event_type}"
   end
 end
 
-GLib::MainLoop.new(nil, true).run
+main_loop.run
