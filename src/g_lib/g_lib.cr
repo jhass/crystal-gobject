2296ff6b1b2222b83e262c38555fc9eaa8f0b847
Jonne Haß <me@jhass.eu>
2020-06-22 13:32:54 +0200

    wrap GLib.timeout block and GLib.idle_add block into trampoline, fixes #70

--- src/g_lib/g_lib.cr
+++ src/g_lib/g_lib.cr
@@ -6,20 +6,22 @@ require "../closure_data_manager"
 
 module GLib
   def self.idle_add(&block : -> Bool)
+    trampoline = LibGLib::SourceFunc.new { block.call ? 1 : 0 }
     LibGLib.idle_add(
       LibGLib::PRIORITY_DEFAULT_IDLE,
-      LibGLib::SourceFunc.new(block.pointer, Pointer(Void).null),
-      GObject::ClosureDataManager.register(block.closure_data),
+      LibGLib::SourceFunc.new(trampoline.pointer, Pointer(Void).null),
+      GObject::ClosureDataManager.register(trampoline.closure_data),
       ->GObject::ClosureDataManager.deregister
     )
   end
 
   def self.timeout(seconds, &block : -> Bool)
+    trampoline = LibGLib::SourceFunc.new { block.call ? 1 : 0 }
     LibGLib.timeout_add_seconds(
       LibGLib::PRIORITY_DEFAULT_IDLE,
       UInt32.new(seconds),
-      LibGLib::SourceFunc.new(block.pointer, Pointer(Void).null),
-      GObject::ClosureDataManager.register(block.closure_data),
+      LibGLib::SourceFunc.new(trampoline.pointer, Pointer(Void).null),
+      GObject::ClosureDataManager.register(trampoline.closure_data),
       ->GObject::ClosureDataManager.deregister
     )
   end
